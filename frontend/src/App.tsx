51024657a1378ac89bf1fb0222e62d5b00e5468b
azabiyaliev <aza.biyaliev@gmail.com>
2025-02-22 00:28:04 +0600

    [UI] Добавлен ProtectedRoute.tsx.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,21 +7,35 @@ import Cocktails from "./features/cocktails/containers/Cocktails.tsx";
 import CocktailDetails from "./features/cocktails/containers/CocktailDetails.tsx";
 import MyCocktails from "./features/cocktails/containers/MyCocktails.tsx";
 import NewCocktail from "./features/cocktails/containers/NewCocktail.tsx";
+import {useAppSelector} from "./app/hooks.ts";
+import {selectUser} from "./features/users/usersSlice.ts";
+import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute.tsx";
 
 const App = () => {
+
+    const user = useAppSelector(selectUser);
+
 
     return (
         <>
             <NavBar/>
             <Routes>
                 <Route path="/*" element={<Typography variant="h4" sx={{textAlign: "center"}}>Not found </Typography>}/>
-                <Route path="/" element={<Cocktails/>}/>
                 <Route path="/register" element={<RegisterPage/>}/>
                 <Route path="/login" element={<LoginPage/>}/>
+                <Route path="/" element={<Cocktails/>}/>
                 <Route path="/cocktails" element={<Cocktails/>}/>
                 <Route path="/cocktails/:idCocktail" element={<CocktailDetails/>}/>
-                <Route path="/myCocktails" element={<MyCocktails/>}/>
-                <Route path="/addNewCocktail" element={<NewCocktail/>}/>
+                <Route path="/myCocktails" element={
+                    <ProtectedRoute isAllowed={user && (user.role === "admin" || user.role === "user")}>
+                        <MyCocktails/>
+                    </ProtectedRoute>
+                }/>
+                <Route path="/addNewCocktail" element={
+                    <ProtectedRoute isAllowed={user && (user.role === "admin" || user.role === "user")}>
+                        <NewCocktail/>
+                    </ProtectedRoute>
+                }/>
             </Routes>
         </>
     )
